bd19fda968c389652f4b5cebfecd88a31cc3efff
E Kennedy <ekennedy103@gmail.com>
2022-09-21 21:12:10 -0400

    Camera now following player

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    [SerializeField] GameObject followTarget;
+    [SerializeField] private float moveSpeed = 20.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (followTarget != null)
+        {
+            transform.position = Vector3.Lerp(transform.position, followTarget.transform.position, Time.deltaTime * moveSpeed);
+        }
+    }
+}
