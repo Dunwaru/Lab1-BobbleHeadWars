e8454851e30cd16de4e535be509a904a7c455ee6
E Kennedy <ekennedy103@gmail.com>
2022-10-04 21:31:29 -0400

    Chapter 6 - Added logic to the walls

---
+++ Assets/Scripts/ArenaWall.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaWall : MonoBehaviour
+{
+    private Animator arenaAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject arena = transform.parent.gameObject;
+        arenaAnimator = arena.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        arenaAnimator.SetBool("IsLowered", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        arenaAnimator.SetBool("IsLowered", false);
+    }
+}
