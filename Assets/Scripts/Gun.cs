f3af3fe0f902f1c8d0a9e1659e7287d9574cb2de
E Kennedy <ekennedy103@gmail.com>
2022-10-07 10:43:23 -0400

    Chapter 7 - Added Gun upgrade

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -9,6 +9,10 @@ public class Gun : MonoBehaviour
 
     private AudioSource audioSource;
 
+    public bool isUpgraded;
+    public float upgradeTime = 10.0f;
+    private float currentTime;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -33,9 +37,40 @@ public class Gun : MonoBehaviour
 
     void fireBullet()
     {
+
+        Rigidbody bullet = createBullet();
+        bullet.velocity = transform.parent.forward * 100;
+        if (isUpgraded)
+        {
+            Rigidbody bullet2 = createBullet();
+            bullet2.velocity = (transform.right + transform.forward / 0.5f) * 100;
+
+            Rigidbody bullet3 = createBullet();
+            bullet3.velocity = ((transform.right * -1) + transform.forward / 0.5f) * 100;
+        }
+
+        if (isUpgraded)
+        {
+            audioSource.PlayOneShot(SoundManager.Instance.upgradedGunFire);
+        }
+        else
+        {
+            audioSource.PlayOneShot(SoundManager.Instance.gunFire);
+        }
+
+    }
+
+    private Rigidbody createBullet()
+    {
         GameObject bullet = Instantiate(bulletPrefab) as GameObject;
         bullet.transform.position = launchPosition.position;
-        bullet.GetComponent<Rigidbody>().velocity = transform.parent.forward * 100;
-        audioSource.PlayOneShot(SoundManager.Instance.gunFire);
+
+        return bullet.GetComponent<Rigidbody>();
+    }
+
+    public void UpgradeGun()
+    {
+        isUpgraded = true;
+        currentTime = 0;
     }
 }
